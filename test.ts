fecc7126ea9839a1ce053f649e740ceb354efb01
Jovan <jamc015lol@gmail.com>
2022-09-05 20:25:07 +0800

    Update test files

--- test.ts
+++ test.ts
@@ -1,55 +1,63 @@
-import assert from 'assert';
-import envRes from './index.js';
+import assert from "assert";
+import envRes from "./index.js";
 
+const KEY: string = "foo";
+const VALUE: string = "bar";
+const contains: { [id: string]: any } = {
+  foo: "bar",
+  "env-res": "environment-reserve",
+  "just-a-number": 100821,
+};
+const k_v: any[] = Object.entries(contains);
 
-const KEY:string = "foo";
-const VALUE:string = "bar";
+describe("env-res", () => {
+  it("can set a key-value pairs and size of envRes will change", () => {
+    assert.equal(0, envRes.size);
+    for (const [key, value] of k_v) envRes.set(key, value);
+    assert.equal(k_v.length, envRes.size);
+  });
 
-describe('env-res', () => {
-	it("can set a key-value pair", () => {
-		envRes.set(KEY, VALUE);
-		assert.equal(1, 1);
-	});
+  it("can iterate values", () => {
+    let count: number = 0;
+    const count_values: number = k_v.length;
+    const count_iterator: number = 4;
 
-	it("should have size 1 value", () => {
-		assert.equal(1, envRes.size);
-	});
+    envRes.forEach((value: string, key: string) => {
+      assert.notEqual(-1, Object.keys(contains).indexOf(key));
+      assert.notEqual(-1, Object.values(contains).indexOf(value));
+      count++;
+    });
 
-	it("can iterate values", () => {
-		let count:number = 0;
+    for (const [key, value] of envRes.entries()) {
+      assert.notEqual(-1, Object.keys(contains).indexOf(key));
+      assert.notEqual(-1, Object.values(contains).indexOf(value));
+      count++;
+    }
 
-		envRes.forEach((value:string, key:string) => {
-			assert.strictEqual(KEY, key);
-			assert.strictEqual(VALUE, value);
-			count++;
-		});
+    for (const key of envRes.keys()) {
+      assert.notEqual(-1, Object.keys(contains).indexOf(key));
+      count++;
+    }
 
-		for(const obj of envRes.entries()){
-			assert.equal(KEY, obj[0]);
-			assert.equal(VALUE, obj[1]);
-			count++;
-		}
+    for (const value of envRes.values()) {
+      assert.notEqual(-1, Object.values(contains).indexOf(value));
+      count++;
+    }
 
-		for(const obj of envRes.keys()){
-			assert.equal(KEY, obj);
-			count++;
-		}
+    assert.equal(count_iterator * count_values, count);
+  });
 
-		for(const obj of envRes.values()){
-			assert.equal(VALUE, obj);
-			count++;
-		}
+  it("should have value", () => {
+    assert.strictEqual(VALUE, envRes.get(KEY));
+  });
 
-		assert.equal(4, count);
-
-	});
-
-	it("should have value", () => {
-		assert.strictEqual(VALUE, envRes.get(KEY));
-	});
-
-	it("can delete the value", () => {
-		envRes.delete(KEY);
-		assert.equal(0, envRes.size);
-	});
+  it("can delete the value", () => {
+    let count: number = k_v.length;
+    for (const [key, value] of k_v) {
+      assert.equal(count, envRes.size);
+      envRes.delete(key);
+      count--;
+    }
+    assert.equal(0, envRes.size);
+  });
 });
