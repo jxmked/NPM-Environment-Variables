6d8303b5d520fafc7f3049c197abc74c2b66fc47
jxmked <jamc015lol@gmail.com>
2022-09-21 22:28:04 +0800

    Added Support for Window Object

--- index.ts
+++ index.ts
@@ -6,12 +6,30 @@
 
 declare global {
 	var __ENVIRONMENT_VARIABLES__:Map<any, any>;
+	interface Window { __ENVIRONMENT_VARIABLES__:Map<any, any> }
 }
 
-global.__ENVIRONMENT_VARIABLES__ = global.__ENVIRONMENT_VARIABLES__ || new Map();
+const isWindow:boolean = (function(){
+	const check:Function = ():boolean => {
+		try {
+			return typeof window !== void 0 && typeof window.document !== void 0;
+		} catch(err:any) {
+			return false;
+		}
+	}
 
-(module).exports = global.__ENVIRONMENT_VARIABLES__;
-export default global.__ENVIRONMENT_VARIABLES__;
+	return check();
+}());
+
+
+type R = typeof window | typeof global;
+
+const e:R = (isWindow) ? window : global;
+
+e.__ENVIRONMENT_VARIABLES__ = e.__ENVIRONMENT_VARIABLES__ || new Map();
+
+(module).exports = e.__ENVIRONMENT_VARIABLES__;
+export default e.__ENVIRONMENT_VARIABLES__;
 
 /**
 * Written by Jovan De Guia
